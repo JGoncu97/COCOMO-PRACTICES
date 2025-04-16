7ad035c43b319ccac1fabd1e631b989766d8cb06
JGoncu97 <jggc1197@gmail.com>
2025-04-16 15:26:27 -0500

    Modificando importancion de Developers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Home } from './page/home/Home';
-import {Developers} from './page/developers/Developers.jsx';
 import { Documentation } from './page/documentation/Documentation';
 import { Application } from './page/application/Application.jsx';
+import { Developers } from './page/developers/Developers.jsx';
 
 export const App = () => {
   return (
